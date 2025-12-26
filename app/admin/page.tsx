994da86c645615c4656262a3cc22d2fe03faf30b
HungNguyen-bit <nguyenhung20042019@gmail.com>
2025-12-26 21:50:35 +0700

    first commit

---
+++ app/admin/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function AdminPage() {
+  return (
+    <div style={{ padding: 32 }}>
+      <h1>Trang Admin - Nguyễn Hưng</h1>
+      <p>Đăng nhập để chỉnh sửa thông tin cá nhân và quản lý bài viết.</p>
+      {/* Form đăng nhập và quản lý sẽ được bổ sung ở các bước tiếp theo */}
+    </div>
+  );
+}
